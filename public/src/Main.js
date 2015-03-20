25503ab34097f8fc82bb0f08b0c2be3754c4865b
Liam Fisher <liamfisher1@gmail.com>
2015-03-20 23:47:25 +0000

    refactored AsteroidGame and added require.js logic

--- public/src/Main.js
+++ public/src/Main.js
@@ -2,34 +2,38 @@
  * Created by LiamF on 16/03/2015.
  */
 
-AsteroidGame = {
-    _previousTick: 0,
-    deltaTime: function(){
-        return new Date().getTime() - this._previousTick;
-    },
-    framesPerSecond: 30,
-    // Main constants
-    NOSTATE: 0,
-    PAUSED: 1,
-    PLAYING: 2,
 
-    // Object Constants
-    INBUFFER:0,
-    ALIVE: 1,
-    DEAD: 2,
-    NOTYPE: 0,
-    PLAYERTYPE: 1,
-    ASTEROIDTYPE:2,
-    BULLETTYPE:3,
-    _objectBuffer: [],
-    _currentObjectId: 0,
-    getUniqueObjectId: function(){
-        // Return a unique ID. Doesn't matter if we don't fill in blanks. Always provide a unique one.
-        var ret = this._currentObjectId;
-        this._currentObjectId += 1;
-        return ret;
-    }
+define( function(){
+    AsteroidGame = {
+        _previousTick: 0,
+        deltaTime: function(){
+            return new Date().getTime() - this._previousTick;
+        },
+        framesPerSecond: 30,
+        // Main constants
+        NOSTATE: 0,
+        PAUSED: 1,
+        PLAYING: 2,
+    
+        // Object Constants
+        INBUFFER:0,
+        ALIVE: 1,
+        DEAD: 2,
+        NOTYPE: 0,
+        PLAYERTYPE: 1,
+        ASTEROIDTYPE:2,
+        BULLETTYPE:3,
+        _objectBuffer: [],
+        _currentObjectId: 0,
+        getUniqueObjectId: function(){
+            // Return a unique ID. Doesn't matter if we don't fill in blanks. Always provide a unique one.
+            var ret = this._currentObjectId;
+            this._currentObjectId += 1;
+            return ret;
+        }
 };
+});
+
 
 AsteroidGame.Point = function(x, y){
     this.x = x || 0;
